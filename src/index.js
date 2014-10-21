8c27caf0fe4878e3488e5b442cc6a07c4c2b266a
Pablo Novas <pjnovas@gmail.com>
2014-10-21 19:49:13 -0300

    added cross-compatible module format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 
-window.addEventListener('load', function(){
-  window.pac = require('./pac');
-});
+(function(root, factory) {
+
+  // Set up PAC appropriately for the environment.
+
+  // AMD
+  if (typeof define === 'function' && define.amd) {
+    define(['exports'], function(exports) {
+      root.pac = factory(root, exports);
+    });
+
+  // Node.js or CommonJS. 
+  } else if (typeof exports !== 'undefined') {
+    factory(root, exports);
+
+  // As a browser global.
+  } else {
+    root.pac = factory(root, {});
+  }
+
+}(this, function(root, pac) {
+  
+  var previousPAC = root.pac;
+
+  pac = require('./pac');
+
+  //TODO: Take this version from package.json
+  pac.VERSION = '0.0.1';
+
+  pac.noConflict = function() {
+    root.pac = previousPAC;
+    return this;
+  };
+
+  return pac;
+
+}));
