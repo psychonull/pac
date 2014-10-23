f5fd3c3f5c3f24cfd7b01b0e1700d9072077184d
Matias Arriola <matias.arriola@gmail.com>
2014-10-23 18:52:37 -0300

    empty loader added

---
+++ src/Loader.js
@@ -0,0 +1,16 @@
+
+var EngineComponent = require('./EngineComponent');
+
+module.exports = EngineComponent.extend({
+
+  // events: start, progress, complete
+  // events: start:group, progress:group, complete:group
+  init: function(){
+
+  }
+  // some inspiration:
+  //http://www.goodboydigital.com/pixijs/docs/classes/AssetLoader.html
+  //https://github.com/andrewrk/chem/blob/master/lib/resources.js
+  //http://docs.phaser.io/Loader.js.html
+  //http://docs.phaser.io/Cache.js.html
+});
--- src/pac.js
+++ src/pac.js
@@ -6,8 +6,9 @@ module.exports = {
   Scene: require('./Scene'),
 
   Renderer: require('./Renderer'),
+  Loader: require('./Loader'),
 
   Game: require('./Game'),
   create: require('./Game').create
-  
+
 };
