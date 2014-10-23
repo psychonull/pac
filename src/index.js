0a4af708af7cc3b479c1f2f12e914cfff342c313
Matias Arriola <matias.arriola@gmail.com>
2014-10-23 02:06:05 -0300

    index.js wrapper fixed for browser and commonjs

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
+module.exports = (function(root) {
+  root = root || {};
 
-(function(root, factory) {
-
-  // Set up PAC appropriately for the environment.
-
-  // AMD
-  if (typeof define === 'function' && define.amd) {
-    define(['exports'], function(exports) {
-      root.pac = factory(root, exports);
-    });
-
-  // Node.js or CommonJS. 
-  } else if (typeof exports !== 'undefined') {
-    factory(root, exports);
-
-  // As a browser global.
-  } else {
-    root.pac = factory(root, {});
-  }
-
-}(this, function(root, pac) {
-  
   var previousPAC = root.pac;
 
   pac = require('./pac');
 
-  //TODO: Take this version from package.json
-  pac.VERSION = '0.0.1';
+  pac.VERSION = require('../package.json').version;
 
   pac.noConflict = function() {
     root.pac = previousPAC;
@@ -34,4 +14,4 @@
 
   return pac;
 
-}));
+})(window);
--- test/specs/index.js
+++ test/specs/index.js
@@ -1,9 +1,24 @@
+var chai = require('chai');
+var expect = chai.expect;
 
 describe('PAC', function(){
 
+  describe('Pac initialization', function(){
+    var pac = require('../../src');
+
+    it('must contain the version', function(){
+      expect(pac.VERSION).to.be.ok;
+    });
+
+    //TODO: how to test this?
+    it('must have noConflict()', function(){
+      expect(pac.noConflict).to.be.a('function');
+    });
+  });
+
   require('./base');
   require('./Game/index.js');
-  
+
   require('./Scene/index.js');
   require('./Scenes/index.js');
 
