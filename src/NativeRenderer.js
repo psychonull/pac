025445740fab9e287e627140e90f71fa63bf0515
Matias Arriola <matias.arriola@gmail.com>
2014-11-10 18:13:12 -0300

    Text implementation for NativeRenderer

--- src/NativeRenderer.js
+++ src/NativeRenderer.js
@@ -1,5 +1,7 @@
 
-var Renderer = require('./Renderer');
+var Renderer = require('./Renderer'),
+  Sprite = require('./Sprite'),
+  Text = require('./Text');
 
 var NativeRenderer = module.exports = Renderer.extend({
 
@@ -44,11 +46,13 @@ var NativeRenderer = module.exports = Renderer.extend({
 
     this.stage.get(layer).each(function(obj, index){
 
-      var textures = this.game.cache.images;
+      if(obj instanceof Sprite){
+        var textures = this.game.cache.images;
 
-      var texture = textures.get(obj.texture);
-      obj.image = texture.raw();
-      obj.frames = texture.frames;
+        var texture = textures.get(obj.texture);
+        obj.image = texture.raw();
+        obj.frames = texture.frames;
+      }
 
     }, this);
 
@@ -71,24 +75,75 @@ var NativeRenderer = module.exports = Renderer.extend({
       var layer = this.stage.get(name);
 
       layer.each(function(o){
-
-        if (o.frame !== null && o.frames){
-          var frm = o.frames.get(o.frame);
-
-          this.context.drawImage(o.image,
-            frm.x, frm.y, frm.width, frm.height,
-            o.position.x, o.position.y, o.size.width, o.size.height);
+        if(o instanceof Sprite){
+          this._renderSprite(o);
         }
-        else {
-
-          this.context.drawImage(o.image,
-            o.position.x, o.position.y, o.size.width, o.size.height);
+        else if(o instanceof Text){
+          this._renderText(o);
         }
 
       }, this);
 
     }, this);
 
+  },
+
+  _renderSprite: function(o){
+    if (o.frame !== null && o.frames){
+      var frm = o.frames.get(o.frame);
+
+      this.context.drawImage(o.image,
+        frm.x, frm.y, frm.width, frm.height,
+        o.position.x, o.position.y, o.size.width, o.size.height);
+    }
+    else {
+      this.context.drawImage(o.image,
+        o.position.x, o.position.y, o.size.width, o.size.height);
+    }
+  },
+
+  _renderText: function(o){
+    var ctx = this.context;
+    ctx.textBaseline = 'top';
+    ctx.font = o.font;
+    ctx.fillStyle = o.fill;
+    ctx.lineWidth = o.strokeThickness;
+    ctx.strokeStyle = o.stroke;
+    if(o.wordWrap){
+      this._wrapTextAndDraw(o);
+    }
+    else {
+      ctx.fillText(o.value, o.position.x, o.position.y);
+      if(o.strikeThickness){
+        ctx.strokeText(o.value, o.position.x, o.position.y);
+      }
+    }
+    ctx.restore();
+  },
+
+  _wrapTextAndDraw: function(o){
+    var words = o.value.split(' ');
+    var line = '';
+    var x = o.position.x,
+      y = o.position.y;
+    for(var n = 0; n < words.length; n++) {
+      var testLine = line + words[n] + ' ';
+      var metrics = this.context.measureText(testLine);
+      var testWidth = metrics.width;
+      if (testWidth > o.wordWrap && n > 0) {
+        this.context.fillText(line, x, y);
+        line = words[n] + ' ';
+        //TODO: Provide proper lineHeight instead of this ugly hack.
+        y += parseInt(o.font, 10) + 2;
+      }
+      else {
+        line = testLine;
+      }
+    }
+    this.context.fillText(line, x, y);
+    if(o.strikeThickness){
+      this.context.strokeText(o.value, o.position.x, o.position.y);
+    }
   }
 
 });
