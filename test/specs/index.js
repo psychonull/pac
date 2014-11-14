3d8dade9316e6a776afa7a5e329dc4e8b047bf3c
Pablo Novas <pjnovas@gmail.com>
2014-11-13 23:36:48 -0300

    uncomment tests

--- test/specs/index.js
+++ test/specs/index.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('PAC', function(){
-/*
+
   describe('Pac initialization', function(){
     var pac = require('../../src');
 
@@ -31,14 +31,12 @@ describe('PAC', function(){
   require('./EngineComponents');
   require('./Point.js');
   require('./GameObjects');
-*/
   require('./Animations');
-  /*
   require('./Cache');
   require('./Lists');
   require('./Assets');
   require('./Inputs');
   require('./ActionLists');
   require('./Prefabs');
-*/
+
 });
