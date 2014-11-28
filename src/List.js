b65418a41d053da4e6f75bc0afc8511d86fed828
Matias Arriola <matias.arriola@gmail.com>
2014-11-28 14:55:41 -0300

    list accept list in constructor

--- src/List.js
+++ src/List.js
@@ -11,14 +11,18 @@ var List = module.exports = Emitter.extend({
 
   init: function(items, options){
 
-    if (items && !Array.isArray(items)){
-      throw new Error('invalid argument, expected an array');
+    if (items && (!Array.isArray(items) && (!(items instanceof List)))){
+      throw new Error('invalid argument, expected an array or List');
     }
 
     this._items = [];
     this.length = 0;
 
     this.comparator = (options && options.comparator) || this.comparator;
+
+    if(items instanceof List){
+      items = items._items;
+    }
 
     if (items && items.length > 0){
       items.forEach(this._set.bind(this));
