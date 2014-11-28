e9b2a9a71de8e1e19525dd880df2574fc78dade8
Matias Arriola <matias.arriola@gmail.com>
2014-11-28 13:47:11 -0300

    Text wrapToScreen, Speak dynamic duration and wrap based on content

--- src/actions/Speak.js
+++ src/actions/Speak.js
@@ -18,12 +18,24 @@ module.exports = Action.extend({
     this.text = (options && options.text) || this.text;
     this.duration = (options && options.duration) || this.duration;
     this.after = (options && options.after) || this.after;
+    this.duration += this.text.length * 0.05;
   },
 
   onStart: function() {
     this.elapsed = 0;
     var owner = this.actions.owner;
     owner.speakerText.value = this.text;
+    if(owner.speakerText.wordWrap){
+      if(this.text.length < 50){
+        owner.speakerText.wordWrap = 120;
+      }
+      else if(this.text.length < 100 ){
+        owner.speakerText.wordWrap = 170;
+      }
+      else{
+        owner.speakerText.wordWrap = 220;
+      }
+    }
   },
 
   onEnd: function() {
@@ -36,7 +48,8 @@ module.exports = Action.extend({
 
   update: function(dt) {
     this.elapsed += dt;
-    if(this.elapsed >= this.duration){
+    if(this.elapsed >= this.duration ||
+        this.actions.owner.game.inputs.cursor.isDown){
       this.isFinished = true;
     }
   }
